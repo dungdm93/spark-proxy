f7a3bdfc79432845c94d50409a88b24322f89948
Đặng Minh Dũng <dungdm93@live.com>
2023-11-08 09:47:55 +0700

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    id("scala")
+}
+
+group = "org.apache.spark"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
