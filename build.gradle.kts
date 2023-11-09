bca6e95119b487a9b686d24ad58e6784ce23b2b4
Đặng Minh Dũng <dungdm93@live.com>
2023-11-09 09:21:06 +0700

    add SparkOperator CRDs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     scala
+    id("io.fabric8.java-generator") version "6.9.2"
 }
 
 group = "org.apache.spark"
@@ -10,6 +11,7 @@ val scalaBinaryVersion = "2.12"
 val sparkVersion = "3.5.0"
 val hadoopVersion = "3.3.4"
 val jettyVersion = "9.4.51.v20230217"
+val fabric8Version = "6.9.2"
 
 repositories {
     mavenCentral()
@@ -19,6 +21,8 @@ dependencies {
     implementation("org.apache.spark:spark-core_$scalaBinaryVersion:$sparkVersion")
     implementation("org.apache.hadoop:hadoop-aws:$hadoopVersion")
     implementation("org.eclipse.jetty:jetty-servlet:$jettyVersion")
+    implementation("io.fabric8:kubernetes-client:$fabric8Version")
+    implementation("io.fabric8:generator-annotations:$fabric8Version")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -32,6 +36,23 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(8))
     }
+}
+
+javaGen {
+    source = file("src/main/resources/kubernetes")
+    target = file("build/generated/sources/crds/java/main")
+    // javax.annotation.processing.Generated requires Java 9
+    generatedAnnotations = false
+}
+
+sourceSets.main {
+    java {
+        srcDir("build/generated/sources/crds/java/main")
+    }
+}
+
+tasks.withType<JavaCompile> {
+    dependsOn("crd2java")
 }
 
 tasks.test {
