b55dba402ce89a85b187f80f377a0f07140f8b64
Đặng Minh Dũng <dungdm93@live.com>
2023-11-16 14:01:57 +0700

    deduplicate code in ProxyPage

--- src/main/scala/org/apache/spark/deploy/history/ProxyPage.scala
+++ src/main/scala/org/apache/spark/deploy/history/ProxyPage.scala
@@ -1,6 +1,5 @@
 package org.apache.spark.deploy.history
 
-import org.apache.spark.status.api.v1.ApplicationInfo
 import org.apache.spark.ui.{UIUtils, WebUIPage}
 
 import javax.servlet.http.HttpServletRequest
@@ -9,88 +8,62 @@ import scala.xml.Node
 private[history] class ProxyPage(pageSize: Int, proxyProvider: ApplicationProxyProvider, historyProvider: ApplicationHistoryProvider)
   extends WebUIPage("") {
   def render(request: HttpServletRequest): Seq[Node] = {
-    val content = scripts(request) ++ renderInfo() ++ renderRunningApplications() ++ renderCompletedApplications()
+    val content = scripts(request) ++ renderInfo() ++ renderApplications("running-app") ++ renderApplications("completed-app")
     UIUtils.basicSparkPage(request, content, "Proxy Server", true)
   }
 
   private def scripts(request: HttpServletRequest): Seq[Node] = {
+    // @formatter:off
     <script src={UIUtils.prependBaseUri(request, "/static/utils.js")}></script>
-      <script src={UIUtils.prependBaseUri(request, "/static/dataTables.rowsGroup.js")}></script>
-      <script src={UIUtils.prependBaseUri(request, "/static-proxy/proxypage.js")}></script>
-      <script>setAppLimit(
-        {pageSize}
-        )</script>
+    <script src={UIUtils.prependBaseUri(request, "/static/dataTables.rowsGroup.js")}></script>
+    <script src={UIUtils.prependBaseUri(request, "/static/historypage-common.js")}></script>
+    <script src={UIUtils.prependBaseUri(request, "/static-proxy/proxypage.js")}></script>
+    <script>
+      setAppLimit({pageSize})
+    </script>
+    // @formatter:off
   }
 
   private def renderInfo(): Seq[Node] = {
     val lastUpdatedTime = historyProvider.getLastUpdatedTime()
     val historyProviderConfig = historyProvider.getConfig()
     val proxyProviderConfig = proxyProvider.getConfig()
-    val eventLogsUnderProcessCount = 100
+    val eventLogsUnderProcessCount = historyProvider.getEventLogsUnderProcess()
 
+    // @formatter:off
     <div>
       <ul class="list-unstyled">
-        {historyProviderConfig.map { case (k, v) =>
-        <li>
-          <strong>
-            {k}
-            :</strong>{v}
-        </li>
-      }}{proxyProviderConfig.map { case (k, v) =>
-        <li>
-          <strong>
-            {k}
-            :</strong>{v}
-        </li>
-      }}
-      </ul>{if (eventLogsUnderProcessCount > 0) {
+        {historyProviderConfig.map { case (k, v) => <li><strong>{k}:</strong> <code>{v}</code></li> }}
+        {if (lastUpdatedTime > 0) {
+        <li><strong>Last updated:</strong> <code id="last-updated">{lastUpdatedTime}</code></li>
+        }}
+        {proxyProviderConfig.map { case (k, v) => <li><strong>{k}:</strong> <code>{v}</code></li> }}
+        <li><strong>Client timezone:</strong> <code id="time-zone"></code></li>
+      </ul>
+      {if (eventLogsUnderProcessCount > 0) {
       <p>There are
         {eventLogsUnderProcessCount}
         event log(s) currently being processed which may result
         in additional applications getting listed on this page.
         Refresh the page to view updates.
       </p>
-    }}{if (lastUpdatedTime > 0) {
-      <p>Last updated:
-        <span id="last-updated">
-          {lastUpdatedTime}
-        </span>
-      </p>
-    }}{<p>Client local time zone:
-      <span id="time-zone"></span>
-    </p>}
+      }}
     </div>
+    // @formatter:on
   }
 
-  private def renderRunningApplications(): Seq[Node] = {
+  private def renderApplications(name: String): Seq[Node] = {
     <div class="row">
       <div class="col-12">
-        <span id="running-app" class="collapse-aggregated-activeApps collapse-table"
-              onClick="collapseTable('collapse-aggregated-activeApps','aggregated-activeApps')">
+        <span id={name} class={s"collapse-table collapse-aggregated-${name}"}
+              onClick={s"collapseTable('collapse-aggregated-${name}', 'aggregated-${name}')"}>
           <h4>
             <span class="collapse-table-arrow arrow-open"></span>
-            <a id="running-app-title">Running Applications (0)</a>
+            <a id={s"$name-title"}>Running Applications (0)</a>
           </h4>
         </span>
-        <div class="aggregated-activeApps collapsible-table">
-          <div id="running-app-table"></div>
-        </div>
-      </div>
-    </div>
-  }
-
-  private def renderCompletedApplications(): Seq[Node] = {
-    <div class="row">
-      <div class="col-12">
-        <span id="completed-app" class="collapse-aggregated-completedApps collapse-table"
-              onClick="collapseTable('collapse-aggregated-completedApps', 'aggregated-completedApps')">
-          <h4>
-            <span class="collapse-table-arrow arrow-open"></span>
-            <a id="completed-app-title">Completed Applications (0)</a>
-          </h4>
-        </span>
-        <div class="aggregated-completedApps collapsible-table">
-          <div id="completed-app-table"></div>
+        <div class={s"collapsible-table aggregated-${name}"}>
+          <div id={s"$name-table"}></div>
         </div>
       </div>
     </div>
