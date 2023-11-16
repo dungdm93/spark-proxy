e3260538bbcd956dc937a6ac8b61549cc823a309
Đặng Minh Dũng <dungdm93@live.com>
2023-11-16 14:39:07 +0700

    config kubernetes

--- src/main/scala/org/apache/spark/deploy/history/Proxy.scala
+++ src/main/scala/org/apache/spark/deploy/history/Proxy.scala
@@ -2,9 +2,21 @@ package org.apache.spark.deploy.history
 
 import org.apache.spark.internal.config.ConfigBuilder
 
+//noinspection TypeAnnotation
 private[spark] object Proxy {
   val PROVIDER = ConfigBuilder("spark.proxy.provider")
-    .version("1.1.0")
+    .version("1.0.0")
     .stringConf
     .createOptional
+
+  val KUBERNETES_NAMESPACE = ConfigBuilder("spark.proxy.kubernetes.namespace")
+    .version("1.0.0")
+    .stringConf
+    .createOptional
+
+  val KUBERNETES_RESYNC_PERIOD = ConfigBuilder("spark.proxy.kubernetes.resyncPeriodMs")
+    .version("1.0.0")
+    .longConf
+    .checkValue(_ > 1000L, "resyncPeriod need to be > 1000L")
+    .createWithDefault(30 * 1000)
 }
--- src/main/scala/org/apache/spark/kubernetes/KubernetesProxyProvider.scala
+++ src/main/scala/org/apache/spark/kubernetes/KubernetesProxyProvider.scala
@@ -3,7 +3,7 @@ package org.apache.spark.kubernetes
 import io.fabric8.kubernetes.client.KubernetesClientBuilder
 import io.k8s.sparkoperator.v1beta2.SparkApplication
 import org.apache.spark.SparkConf
-import org.apache.spark.deploy.history.ApplicationProxyProvider
+import org.apache.spark.deploy.history.{ApplicationProxyProvider, Proxy}
 import org.apache.spark.status.api.v1.ApplicationInfo
 
 import scala.collection.mutable
@@ -11,12 +11,15 @@ import scala.collection.mutable
 class KubernetesProxyProvider(conf: SparkConf)
   extends ApplicationProxyProvider {
   private val client = new KubernetesClientBuilder().build()
+  private val namespace = conf.get(Proxy.KUBERNETES_NAMESPACE)
   private val informerFactory = client.informers()
   private val nameToId = new mutable.HashMap[String, String]()
   private val idToInfo = new mutable.HashMap[String, SparkAppInfo]()
 
   override def start(): Unit = {
-    val informer = informerFactory.sharedIndexInformerFor(classOf[SparkApplication], 30 * 1000)
+    val resyncPeriod = conf.get(Proxy.KUBERNETES_RESYNC_PERIOD)
+    val informer = informerFactory.inNamespace(namespace.orNull)
+      .sharedIndexInformerFor(classOf[SparkApplication], resyncPeriod)
     val podGetter = client.pods()
     val handler = new SparkApplicationHandler(podGetter, this)
     informer.addEventHandler(handler)
@@ -28,12 +31,11 @@ class KubernetesProxyProvider(conf: SparkConf)
     informerFactory.stopAllRegisteredInformers()
   }
 
-  override def getConfig(): Map[String, String] = {
+  override def getConfig(): Map[String, String] =
     Map(
-      "Spark Proxy" -> "Kubernetes",
-      "Namespace" -> "default"
+      "Spark proxy" -> s"k8s://${client.getConfiguration.getMasterUrl}",
+      "Namespace" -> namespace.getOrElse("<global>"),
     )
-  }
 
   override def getAddress(id: String): Option[String] =
     idToInfo.get(id).map(info => s"http://${info.driverInfo.host}:${info.driverInfo.port}")
