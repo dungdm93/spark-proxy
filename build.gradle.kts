e39c9379d9499496f6758e57b2ef2cb64384d5ad
Đặng Minh Dũng <dungdm93@live.com>
2023-11-17 10:44:37 +0700

    migrate from `io.fabric8.java-generator` to `org.jsonschema2pojo`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     scala
-    id("io.fabric8.java-generator") version "6.9.2"
+    id("org.jsonschema2pojo") version "1.2.1"
 }
 
 group = "org.apache.spark"
@@ -12,6 +12,8 @@ val sparkVersion = "3.5.0"
 val hadoopVersion = "3.3.4"
 val jettyVersion = "9.4.51.v20230217"
 val fabric8Version = "6.9.2"
+val lombokVersion = "1.18.30"
+val sundrVersion = "0.101.3"
 
 repositories {
     mavenCentral()
@@ -25,8 +27,18 @@ dependencies {
     implementation("io.fabric8:kubernetes-client:$fabric8Version")
     implementation("io.fabric8:generator-annotations:$fabric8Version")
 
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("io.sundr:builder-annotations:$sundrVersion")
+    annotationProcessor("io.sundr:transform-annotations:$sundrVersion")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+buildscript {
+    dependencies {
+        classpath("io.fabric8:kubernetes-model-jsonschema2pojo:6.9.2")
+    }
 }
 
 scala {
@@ -39,21 +51,23 @@ java {
     }
 }
 
-javaGen {
-    source = file("src/main/resources/kubernetes")
-    target = file("build/generated/sources/crds/java/main")
-    // javax.annotation.processing.Generated requires Java 9
-    generatedAnnotations = false
-}
+// See: https://github.com/fabric8io/kubernetes-client/blob/v6.9.2/pom.xml#L1057-L1067
+// Ref: https://github.com/joelittlejohn/jsonschema2pojo/tree/master/jsonschema2pojo-gradle-plugin
+jsonSchema2Pojo {
+    sourceFiles = files("src/main/resources/schema")
+    targetDirectory = file("src/gen/java")
+    targetPackage = "io.k8s.sparkoperator.api.model"
+    removeOldOutput = true
 
-sourceSets.main {
-    java {
-        srcDir("build/generated/sources/crds/java/main")
-    }
-}
-
-tasks.withType<JavaCompile> {
-    dependsOn("crd2java")
+    setAnnotationStyle("NONE") // disable to avoid duplicate with fabric8
+    setCustomAnnotator("io.fabric8.kubernetes.jsonschema2pojo.ExtensionTypeAnnotator")
+    setCustomRuleFactory("io.fabric8.kubernetes.jsonschema2pojo.Fabric8RuleFactory")
+    includeJsr303Annotations = true
+    includeJsr305Annotations = true
+    includeConstructors = true
+    // use lombok instead
+    includeToString = false
+    includeHashcodeAndEquals = false
 }
 
 tasks.test {
